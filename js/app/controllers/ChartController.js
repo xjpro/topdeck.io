788170c383d6ad10fd20d2442a6cb96ec058525e
jprodahl <jprodahl@gmail.com>
2013-11-27 23:57:54 -0600

    Moved toggle for draw chance Put stats on right side instead of graphs various other improvements

--- js/app/controllers/ChartController.js
+++ js/app/controllers/ChartController.js
@@ -1,18 +1,19 @@
 app.controller("ChartController", ["$scope", "Deck", function($scope, Deck) {
 
     $scope.count = function() {
-        return _(Deck.cards).pluck('quantity').reduce(function(sum, num) { return sum + num; }) || 0;
+        return _(Deck.cards).pluck("quantity").reduce(function(sum, num) { return sum + num; }) || 0;
     };
     $scope.showDrawChance = true;
-    $scope.showLineAttack = true;
-    $scope.showLineHealth = true;
+    $scope.showLineAttack = false;
+    $scope.showLineHealth = false;
+
+    $scope.startingOptions = [{ label: "first", value: 3 }, { label: "second", value: 4 }];
+    $scope.startingHand = 3;
     var turns = 10;
-    var startingHand = 3;
     var deckSize = 30;
 
-    $scope.$watch(function() { return $scope.count() + $scope.showDrawChance + $scope.showLineAttack + $scope.showLineHealth; }, function() {
-
-        //console.log(Deck.cards);
+    $scope.$watch(function() { return $scope.count() + $scope.startingHand +
+        $scope.showDrawChance + $scope.showLineAttack + $scope.showLineHealth; }, function() {
 
         var minionCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         var cardCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -24,7 +25,21 @@ app.controller("ChartController", ["$scope", "Deck", function($scope, Deck) {
             }
         });
 
+        var allCards =_(Deck.cards);
+
         $scope.graphData = {
+            minions: allCards.filter(function(card) { return card.type == "Minion"; })
+                .pluck("quantity").reduce(function(sum, num) { return sum + num; }) || 0,
+            spells: allCards.filter(function(card) { return card.type == "Spell"; })
+                .pluck("quantity").reduce(function(sum, num) { return sum + num; }) || 0,
+            weapons:  allCards.filter(function(card) { return card.type == "Weapon"; })
+                .pluck("quantity").reduce(function(sum, num) { return sum + num; }) || 0,
+            battlecries: allCards.filter(function(card) { return card.description && card.description.indexOf("Battlecry:") != -1; })
+                .pluck("quantity").reduce(function(sum, num) { return sum + num; }) || 0,
+            secrets: allCards.filter(function(card) { return card.description && card.description.indexOf("Secret:") != -1; })
+                .pluck("quantity").reduce(function(sum, num) { return sum + num; }) || 0,
+            legendaries: allCards.filter(function(card) { return card.quality == 5; })
+                .pluck("quantity").reduce(function(sum, num) { return sum + num; }) || 0,
             cardCounts: {
                 all: cardCounts,
                 minions: minionCounts,
@@ -38,7 +53,7 @@ app.controller("ChartController", ["$scope", "Deck", function($scope, Deck) {
             for(var i = 0; i <= turns; i++) {
                 //jStat.hypgeom.pdf(0, 60, 4, 7) chance of drawing a 4/60 card on turn one (opening hand being 7 cards)
                 // http://www.kibble.net/magic/magic10.php
-                var cardsDrawn = startingHand + i;
+                var cardsDrawn = $scope.startingHand + i;
                 var chanceCardNotDrawn = jStat.hypgeom.pdf(0, deckSize, cardCounts[i], cardsDrawn);
                 points.push(Math.round(Math.roundTo(1 - chanceCardNotDrawn, 2) * 100));
             }
