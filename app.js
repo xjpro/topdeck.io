1ac9757f56905645297a5733b5a457d50f141bb1
jprodahl <jprodahl@gmail.com>
2013-11-27 18:01:00 -0600

    way to set environment on console and deliver minified js files

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 
+// set environment based on inputs
+if(!process.argv[2] || process.argv[2] == "-prod") {
+    process.env.NODE_ENV = "production";
+}
+else if(process.argv[2] == "-dev") {
+    process.env.NODE_ENV = "development";
+}
+
 // vendor services
-GLOBAL._ = require('lodash-node'); // lo-dash in node, this is awesome
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/deckstats');
+GLOBAL._ = require("lodash-node"); // lo-dash in node, this is awesome
+var mongo = require("mongodb");
+var monk = require("monk");
+var db = monk("localhost:27017/deckstats");
 
 var express = require("express"),
     mainController = require("./server/controllers/main.js")
-    apiController = require('./server/controllers/api.js');
+    apiController = require("./server/controllers/api.js");
 
 var app = express()
     .set("views", "./server/views")
@@ -21,6 +29,6 @@ var app = express()
     .post("/api/decks/:guid", apiController.saveDeck(db))
     .listen(8080);
 
-console.log("Listening on port 8080");
+console.log("Listening on port 8080 in " + process.env.NODE_ENV + " mode");
 
 // http://cwbuecheler.com/web/tutorials/2013/node-express-mongo/
